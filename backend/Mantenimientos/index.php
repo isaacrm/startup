ee9d74782bafcf2b1fff27bc6c9a45134f21c211
isaacrm <mendezisaac.11@gmail.com>
2015-05-16 12:39:18 -0600

    backend update

--- backend/Mantenimientos/index.php
+++ backend/Mantenimientos/index.php
@@ -136,12 +136,6 @@
                 <!--BEGIN CONTENT-->
                 <div class="page-content"></div>
                 <!--END CONTENT-->
-                <!--BEGIN FOOTER-->
-                <div id="footer">
-                    <div class="copyright">
-                        <a href="http://themifycloud.com">2014 © KAdmin Responsive Multi-Purpose Template</a></div>
-                </div>
-                <!--END FOOTER-->
             </div>
             <!--END PAGE WRAPPER-->
         </div>
