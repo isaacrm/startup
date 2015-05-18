8c46974d44a5bf7215b5f41df63669aad593b169
isaacrm <mendezisaac.11@gmail.com>
2015-05-18 01:56:05 -0600

    backend update

--- backend/Mantenimientos/topbar.php
+++ backend/Mantenimientos/topbar.php
@@ -9,7 +9,7 @@
                 <li class="dropdown topbar-user"><a data-hover="dropdown" href="#" class="dropdown-toggle"><img src="images/avatar/48.jpg" alt="" class="img-responsive img-circle"/>&nbsp;<span class="hidden-xs">Robert John</span>&nbsp;<span class="caret"></span></a>
                     <ul class="dropdown-menu dropdown-user pull-right">
                         <li><a href="#"><i class="fa fa-user"></i>My Profile</a></li>
-                        <li><a href="logout.php"><i class="fa fa-key"></i>Log Out</a></li>
+                        <li><a href="../../logout.php"><i class="fa fa-key"></i>Log Out</a></li>
                     </ul>
                 </li>
             </ul>
