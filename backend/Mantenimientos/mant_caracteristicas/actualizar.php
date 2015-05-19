694e0bf26eecf5ccb699c116540582b43c46dae6
Karen <joha_kstar1@hotmail.com>
2015-05-18 19:50:00 -0600

    backend_update

---
+++ backend/Mantenimientos/mant_caracteristicas/actualizar.php
@@ -0,0 +1,99 @@
+<?php
+$id = null;
+if(!empty($_GET['id_caracteristica'])) {
+    $id = $_GET['id_caracteristica'];
+}
+if($id == null) {
+    header("Location: caracteristicas.php");
+}
+require("../../bd.php");
+if(!empty($_POST)){
+    // validation errors
+    $tituloError = null;
+    // post values
+    $titulo = $_POST['titulo'];
+
+    // validate input
+    $valid = true;
+    if(empty($titulo)) {
+        $tituloError = "Por favor ingrese un titulo.";
+        $valid = false;
+    }
+
+    // update data
+    if($valid) {
+        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $sql = "UPDATE caracteristicas SET titulo = ? WHERE id_caracteristica = ?";
+        $stmt = $PDO->prepare($sql);
+        $stmt->execute(array($titulo, $id_caracteristica));
+        $PDO = null;
+        header("Location: caracteristicas.php");
+    }
+}
+else {
+    // read data
+    $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "SELECT titulo FROM noticias WHERE id_caracteristica = ?";
+    $stmt = $PDO->prepare($sql);
+    $stmt->execute(array($id_caracteristica));
+    $data = $stmt->fetch(PDO::FETCH_ASSOC);
+    $PDO = null;
+    if(empty($data)) {
+        header("Location: caracteristicas.php");
+    }
+    $titulo = $data['titulo'];
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Winefun | Caracteristicas</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php include '../estilos2.php';?>
+</head>
+<body>
+<div>
+    <!--BEGIN THEME SETTING--><!--END THEME SETTING-->
+    <!--BEGIN BACK TO TOP-->
+    <a id="totop" href="#"><i class="fa fa-angle-up"></i></a>
+    <!--END BACK TO TOP-->
+    <?php include '../topbar.php';?>
+    <div id="wrapper">
+        <?php include '../sidebar.php';?>
+        <!--BEGIN PAGE WRAPPER-->
+        <div id="page-wrapper">
+            <!--BEGIN TITLE & BREADCRUMB PAGE-->
+            <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
+                <div class="page-header pull-left">
+                    <div class="page-title">
+                        Modificar Caracteristicas</div>
+                </div>
+                <div class="clearfix">
+                </div>
+s
+                <form method='POST'>
+                    <div class='form-group <?php print(!empty($tituloError)?"has-error":""); ?>'>
+                        <label for='titulo'>Titulo</label>
+                        <input type='text' name='titulo' placeholder='Titulo' required='required' id='titulo' class='form-control' value='<?php print($titulo); ?>'>
+                        <?php print(!empty($tituloError)?"<span class='help-block'>$tituloError</span>":""); ?>
+                    </div>
+                    <div class='form-actions'>
+                        <button type='submit' class='btn btn-primary'>Actualizar</button>
+                        <a class='btn btn btn-default' href='caracteristicas.php'>Regresar</a>
+                    </div>
+                </form>
+
+            </div>
+            <!--END TITLE & BREADCRUMB PAGE-->
+            <!--BEGIN CONTENT-->
+            <div class="page-content"></div>
+            <!--END CONTENT-->
+        </div>
+        <!--END PAGE WRAPPER-->
+    </div>
+</div>
+<?php include '../funciones2.php';?>
+</body>
+</html>
--- backend/Mantenimientos/mant_caracteristicas/crear.php
+++ backend/Mantenimientos/mant_caracteristicas/crear.php
@@ -2,14 +2,8 @@
 if(!empty($_POST)) {
     // validation errors
     $tituloError = null;
-    $subtituloError = null;
-    $leyendeError = null;
-    $imagenError = null;
     // post values
     $titulo = $_POST['titulo'];
-    $subtitulo = $_POST['subtitulo'];
-    $leyenda = $_POST['leyenda'];
-    $imagen = $_POST['imagen'];
 
     // validate input
     $valid = true;
@@ -17,37 +11,22 @@ if(!empty($_POST)) {
         $tituloError = "Por favor ingrese un titulo.";
         $valid = false;
     }
-
-    if(empty($subtitulo)) {
-        $subtituloError = "Por favor ingrese el subtitulo.";
-        $valid = false;
-    }
-
-    if(empty($leyenda)) {
-        $leyendaError = "Por favor ingrese la leyenda.";
-        $valid = false;
-    }
-
-    if(empty($imagen)) {
-        $imagenError = "Por favor ingrese una imagen.";
-        $valid = false;
-    }
     // insert data
     if($valid) {
         require("../../bd.php");
         $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO noticias(titulo, subtitulo, leyenda, imagen) values(?, ?, ?, ?)";
+        $sql = "INSERT INTO caracteristicas(titulo) values(?)";
         $stmt = $PDO->prepare($sql);
-        $stmt->execute(array($titulo, $subtitulo, $leyenda, $imagen ));
+        $stmt->execute(array($titulo ));
         $PDO = null;
-        header("Location: noticias.php");
+        header("Location: caracteristicas.php");
     }
 }
 ?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
-    <title>Mantenimiento | Noticias</title>
+    <title>Mantenimiento | Caracteristicas</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -68,7 +47,7 @@ if(!empty($_POST)) {
             <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
                 <div class="page-header pull-left">
                     <div class="page-title">
-                        Crear Noticias</div>
+                        Crear Caracteristica</div>
                 </div>
                 <div class="clearfix">
 
@@ -79,21 +58,9 @@ if(!empty($_POST)) {
                             <input type='text' name='titulo' placeholder='Titulo' required='required' id='titulo' class='form-control' value='<?php print(!empty($titulo)?$titulo:""); ?>'>
                             <?php print(!empty($tituloError)?"<span class='help-block'>$tituloError</span>":""); ?>
                         </div>
-                        <div class='form-group <?php print(!empty($subtituloError)?"has-error":""); ?>'>
-                            <input type='text' name='subtitulo' placeholder='Subtitulo' required='required' id='subtitulo' class='form-control' value='<?php print(!empty($subtitulo)?$titulo:""); ?>'>
-                            <?php print(!empty($subtituloError)?"<span class='help-block'>$subtituloError</span>":""); ?>
-                        </div>
-                        <div class='form-group <?php print(!empty($leyendaError)?"has-error":""); ?>'>
-                            <input type='text' name='leyenda' placeholder='Leyenda' required='required' id='leyenda' class='form-control' value='<?php print(!empty($leyenda)?$leyenda:""); ?>'>
-                            <?php print(!empty($leyendaError)?"<span class='help-block'>$leyendaError</span>":""); ?>
-                        </div>
-                        <div class='form-group <?php print(!empty($imagenError)?"has-error":""); ?>'>
-                            <input type='text' name='imagen' placeholder='Imagen' required='required' id='imagen' class='form-control' value='<?php print(!empty($imagen)?$imagen:""); ?>'>
-                            <?php print(!empty($imagenError)?"<span class='help-block'>$imagenError</span>":""); ?>
-                        </div>
                         <div class='form-actions'>
                             <button type='submit' class='btn btn-success'>Crear</button>
-                            <a class='btn btn btn-default' href='noticias.php'>Regresar</a>
+                            <a class='btn btn btn-default' href='caracteristicas.php'>Regresar</a>
                         </div>
                     </form>
                 </div> <!-- /row -->
