26edd7a7a5d0c21360205d6157f2b959f4cb6d9c
isaacrm <mendezisaac.11@gmail.com>
2015-06-21 14:29:29 -0600

    backend updatel

--- backend/Mantenimientos/mant_servicios/actualizar.php
+++ backend/Mantenimientos/mant_servicios/actualizar.php
@@ -49,6 +49,28 @@ if(!empty($_POST)) {
 
     // update data
     if($valid) {
+try {
+    /*Comprueba si hay espacios en blanco*/
+    if (ctype_space($tipo)||ctype_space($descripcion)||ctype_space($precio)){
+        echo"<script type=\"text/javascript\">alert('No se puede dejar datos en blanco');</script>";
+    }
+    /*No cuenta un primer espacio ni un ultimo como caracter*/
+    else if (strlen(trim($tipo, ' ')) <= 1)
+    {
+        echo"<script type=\"text/javascript\">alert('El nombre debe de tener al menos dos caracteres');</script>";
+    }
+    else if (strlen(trim($descripcion, ' ')) <= 4)
+    {
+        echo"<script type=\"text/javascript\">alert('La descripcion debe de tener al menos cinco caracteres');</script>";
+    }
+    /*VAlida solo letras */
+    else if(!preg_match('/^([a-z A-Z ñáéíóú ÑÁÉÍÓÚ Üü ]{2,60})$/i',$tipo)){
+        echo"<script type=\"text/javascript\">alert('El nombre no debe tener números');</script>";
+    }
+    else if(!preg_match('/^\d+(\.(\d{2}))?$/',$precio)){
+        echo"<script type=\"text/javascript\">alert('Formato de precio incorrecto. Debe tener dos decimales. Ej.00.00');</script>";
+    }
+    else {
         $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sql = "UPDATE servicios SET tipo = ?, descripcion = ?, precio = ? WHERE id_servicio = ?";
         $stmt = $PDO->prepare($sql);
@@ -56,13 +78,16 @@ if(!empty($_POST)) {
         $PDO = null;
         header("Location: servicios.php");
     }
-}
-else {
+    } catch (Exception $e) {
+        echo"<script type=\"text/javascript\">alert('Este tipo de usuario ya existe');</script>";
+    }
+    }
+    } else {
     // read data
     $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "SELECT tipo, descripcion, precio FROM servicios WHERE id_servicio = ?";
     $stmt = $PDO->prepare($sql);
-    $stmt->execute(array($id_servicio));
+    $stmt->execute(array($id));
     $data = $stmt->fetch(PDO::FETCH_ASSOC);
     $PDO = null;
     if(empty($data)) {
@@ -105,17 +130,17 @@ else {
                 <form method='POST'>
                     <div class='form-group <?php print(!empty($tipoError)?"has-error":""); ?>'>
                         <label for='tipo'>Tipo</label>
-                        <input type='text' name='tipo' placeholder='Tipo' required='required' id='tipo' class='form-control' value='<?php print($tipo); ?>'>
+                        <input type='text' name='tipo' placeholder='Tipo' required='required' id='tipo' class='form-control' autocomplete="off" maxlength="20" value='<?php print($tipo); ?>'>
                         <?php print(!empty($tipoError)?"<span class='help-block'>$tipoError</span>":""); ?>
                     </div>
                     <div class='form-group <?php print(!empty($descripcionError)?"has-error":""); ?>'>
                         <label for='descripcion'>Descripción</label>
-                        <input type='text' name='descripcion' placeholder='Descripción' required='required' id='descripcion' class='form-control' value='<?php print($descripcion); ?>'>
+                        <input type='text' name='descripcion' placeholder='Descripción' required='required' id='descripcion' class='form-control' autocomplete="off" maxlength="250" value='<?php print($descripcion); ?>'>
                         <?php print(!empty($descripcionError)?"<span class='help-block'>$descripcionError</span>":""); ?>
                     </div>
                     <div class='form-group <?php print(!empty($precioError)?"has-error":""); ?>'>
                         <label for='precio'>Precio</label>
-                        <input type='text' name='precio' placeholder='Precio' required='required' id='precio' class='form-control' value='<?php print($precio); ?>'>
+                        <input type='text' name='precio' placeholder='Precio' required='required' id='precio' class='form-control' autocomplete="off" maxlength="7" value='<?php print($precio); ?>'>
                         <?php print(!empty($precioError)?"<span class='help-block'>$precioError</span>":""); ?>
                     </div>
                     <div class='form-actions'>
