bb2bcaf0152ed59817287d8eb043bbf244531587
isaacrm <mendezisaac.11@gmail.com>
2015-06-21 15:16:24 -0600

    backend updatel

--- backend/Mantenimientos/mant_imagenes_servicios/crear.php
+++ backend/Mantenimientos/mant_imagenes_servicios/crear.php
@@ -11,23 +11,18 @@ if(!isset($_SESSION['alias']))
 ?>
 
 <?php
+require("../../bd.php");
 if(!empty($_POST)) {
     // validation errors
     $urlError = null;
     $tituloError = null;
     $descripcionError = null;
     // post values
-    $url = $_POST['url'];
     $titulo = $_POST['titulo'];
     $descripcion = $_POST['descripcion'];
-
+    $tipo_servicio = $_POST['tipo'];
     // validate input
     $valid = true;
-    if(empty($url)) {
-        $urlError = "Por favor ingrese la url de la imagen.";
-        $valid = false;
-    }
-
 
     if(empty($titulo)) {
         $tituloError = "Por favor ingrese el titulo.";
@@ -40,14 +35,86 @@ if(!empty($_POST)) {
     }
 
     // insert data
+    if (ctype_space($titulo) || ctype_space($descripcion)) {
+        echo"<script type=\"text/javascript\">alert('No se puede dejar datos en blanco');</script>";
+    }
+    else if (strlen(trim($titulo, ' ')) <= 2)
+    {
+        echo"<script type=\"text/javascript\">alert('El nombre debe de tener al menos tres caracteres');</script>";
+    }
+    else if (strlen(trim($descripcion, ' ')) <= 4)
+    {
+        echo"<script type=\"text/javascript\">alert('La descripcion debe de tener al menos cinco caracteres');</script>";
+    }
+    else if(!preg_match('/^([a-z A-Z ñáéíóú ÑÁÉÍÓÚ Üü ]{2,60})$/i',$titulo)){
+        echo"<script type=\"text/javascript\">alert('El titulo no debe tener números');</script>";
+    }
+    else {
     if($valid) {
-        require("../../bd.php");
-        $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $sql = "INSERT INTO imagenes_servicios(url, titulo, descripcion) values(?, ?, ?)";
-        $stmt = $PDO->prepare($sql);
-        $stmt->execute(array($url, $titulo, $descripcion));
-        $PDO = null;
-        header("Location: imagenes_servicios.php");
+        //SUBIR IMAGEN URL
+        if ($_FILES['archivo']['name'] == "") {
+            echo "<script type=\"text/javascript\">alert('Tienes que subir una imagen');</script>";
+        } else {
+            $nombre = $_FILES['archivo']['name'];
+            $nombre_tmp = $_FILES['archivo']['tmp_name'];
+            $tipo = $_FILES['archivo']['type'];
+            $tamano = $_FILES['archivo']['size'];
+
+            $ext_permitidas = array('jpg', 'jpeg', 'gif', 'png');
+            $partes_nombre = explode('.', $nombre);
+            $extension = end($partes_nombre);
+            $ext_correcta = in_array($extension, $ext_permitidas);
+            $tipo_correcto = preg_match('/^image\/(pjpeg|jpeg|gif|png)$/', $tipo);
+            $limite = 2048 * 1024;
+            /*Toma el tamaño de la imagen subida*/
+            $dimensiones = getimagesize($nombre_tmp);
+            $ancho = $dimensiones[0];
+            $alto = $dimensiones[1];
+            /*Compara el tamaño con el que debe de ser*/
+            if ($ancho == 720 && $alto == 480) {
+                /*Compara el peso de la imagen, debe ser menor a 2 MB  (Esto es mas codigo de validacion [extension y tipo])$ext_correcta && $tipo_correcto*/
+                if ($tamano <= $limite) {
+                    if ($_FILES['archivo']['error'] > 0) {
+                        echo 'Error: ' . $_FILES['archivo']['error'] . '<br/>';
+                    } else {
+                        echo 'Nombre: ' . $nombre . '<br/>';
+                        echo 'Tipo: ' . $tipo . '<br/>';
+                        echo 'Tamaño: ' . ($tamano / 1024) . ' Kb<br/>';
+                        echo 'Guardado en: ' . $nombre_tmp;
+
+                        if (file_exists('../img_servicios/' . $nombre)) {
+                            echo '<br/>El archivo ya existe: ' . $nombre;
+                        } else {
+                            $sql = "SELECT MAX(id_servicio) as id_ser FROM servicios";
+                            foreach ($PDO->query($sql) as $row) {
+                                $idservicio = "$row[id_ser]";
+                                $id = $idservicio + 1;
+                            }
+                            move_uploaded_file($nombre_tmp, "../img_servicios/" . $id . ".jpg");
+                            $url = "img_servicios/" . $id . ".jpg";
+                            echo "<br/>Guardado en: " . "../img_servicios/" . $id . ".jpg";
+
+                            /*SELECCIONAR EL ID DEL TIPO DE USUARIO DONDE EL NOMBRE SEA el tipo seleccionado*/
+                            $sql2 = "SELECT id_servicio FROM servicios WHERE tipo='" . $_POST['tipo'] . "'";
+                            foreach ($PDO->query($sql2) as $row2) {
+                                $id_servicio = "$row2[id_servicio]";
+                            }
+                            $PDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                            $sql = "INSERT INTO imagenes_servicios(url, titulo, descripcion, id_servicio) values(?, ?, ?, ?)";
+                            $stmt = $PDO->prepare($sql);
+                            $stmt->execute(array($url, $titulo, $descripcion, $id_servicio));
+                            header("Location: imagenes_servicios.php");
+                        }
+                    }
+                } else {
+                    echo "<script type=\"text/javascript\">alert('La imagen pesa mas de 2 MB');</script>";
+                }
+            } else {
+                echo "<script type=\"text/javascript\">alert('La imagen debe ser exactamende de 720px de alto x 480px de ancho');</script>";
+
+            }
+        }
+    }
     }
 }
 ?>
@@ -81,23 +148,38 @@ if(!empty($_POST)) {
 
                 </div>
                 <div class='row'>
-                    <form method='POST'>
-                        <div class='form-group <?php print(!empty($urlError)?"has-error":""); ?>'>
-                            <input type='text' name='url' placeholder='Url' required='required' id='url' class='form-control' value='<?php print(!empty($url)?$url:""); ?>'>
-                            <?php print(!empty($urlError)?"<span class='help-block'>$urlError</span>":""); ?>
-                        </div>
+                    <form action="#" method="post" class="form" role="form" enctype="multipart/form-data">
                         <div class='form-group <?php print(!empty($tituloError)?"has-error":""); ?>'>
-                            <input type='text' name='titulo' placeholder='Titulo' required='required' id='titulo' class='form-control' value='<?php print(!empty($titulo)?$titulo:""); ?>'>
+                            <input type='text' name='titulo' placeholder='Titulo' required='required' id='titulo' class='form-control' autocomplete="off"  maxlength="25" value='<?php print(!empty($titulo)?$titulo:""); ?>'>
                             <?php print(!empty($tituloError)?"<span class='help-block'>$tituloError</span>":""); ?>
                         </div>
                         <div class='form-group <?php print(!empty($descripcionError)?"has-error":""); ?>'>
-                            <input type='text' name='descripcion' placeholder='Descripción' required='required' id='descripcion' class='form-control' value='<?php print(!empty($descripcion)?$descripcion:""); ?>'>
+                            <input type='text' name='descripcion' placeholder='Descripción' required='required' id='descripcion' class='form-control' autocomplete="off"  maxlength="60" value='<?php print(!empty($descripcion)?$descripcion:""); ?>'>
                             <?php print(!empty($descripcionError)?"<span class='help-block'>$descripcionError</span>":""); ?>
+                        </div>
+                        <div class='form-group'>
+                            <input type="file" name="archivo" id="archivo" accept="image/png, image/jpeg, image/gif"/>
+                        </div>
+                        <div class='form-group'>
+                            <label for='genero'>Tipo de servicio</label>
+                            <select name='tipo' required='required' id='tipo' class='form-control'>
+                                <option ></option>
+                                <?php
+                                $sql = "SELECT tipo FROM servicios ORDER BY id_servicio ASC ";
+                                $data = "";
+                                foreach($PDO->query($sql) as $row) {
+                                    $data .= "<option value= '$row[tipo]'>$row[tipo]</option>";
+                                }
+                                print($data);
+                                $PDO = null;
+                                ?>
+                            </select>
                         </div>
                         <div class='form-actions'>
                             <button type='submit' class='btn btn-success'>Crear</button>
                             <a class='btn btn btn-default' href='imagenes_servicios.php'>Regresar</a>
                         </div>
+
                     </form>
                 </div> <!-- /row -->
             </div>
--- backend/Mantenimientos/mant_imagenes_servicios/imagenes_servicios.php
+++ backend/Mantenimientos/mant_imagenes_servicios/imagenes_servicios.php
@@ -34,7 +34,7 @@ if(!isset($_SESSION['alias']))
             <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
                 <div class="page-header pull-left">
                     <div class="page-title">
-                        Preguntas</div>
+                        Imagenes por servicio</div>
                 </div>
                 <div class="clearfix">
                 </div>
@@ -46,11 +46,15 @@ if(!isset($_SESSION['alias']))
                         <div class='container col-lg-12'>
                             <div class='row'>
                                 <p><a class='btn btn-xs btn-success' href='crear.php'>Crear</a></p>
+                                <form method='POST' action="buscar.php">
+                                    <input  class="col-lg-9" name="buscar" placeholder="Buscar por Titulo" type="text" id='buscar' autocomplete="off" maxlength="60"/>
+                                    <input class="col-lg-3" type="submit" name="submit" value="Buscar">
+                                </form>
                                 <div class="table-responsive">
                                     <table class='table table-striped table-bordered table-hover'>
                                         <tr class='warning '>
                                             <th>ID</th>
-                                            <th>URL</th>
+                                            <th>IMAGEN</th>
                                             <th>TITULO</th>
                                             <th>DESCRIPCION</th>
                                             <th>ACCION</th>
@@ -58,12 +62,26 @@ if(!isset($_SESSION['alias']))
                                         <tbody>
                                         <?php
                                         require("../../bd.php");
-                                        $sql = "SELECT id_imagen, url, titulo, descripcion FROM imagenes_servicios ORDER BY id_imagen ASC";
+                                        require_once("../../libs/Zebra_Pagination.php");
+                                        /*Aqui obtenemos el total de registros*/
+                                        $sql0 = "SELECT COUNT(*) as total_datos FROM imagenes_servicios";
+                                        foreach ($PDO->query($sql0) as $row0) {
+                                            $totaldatos = "$row0[total_datos]";
+                                        }
+                                        /*Numero de registros que se quiere por tabla*/
+                                        $filas = 10;
+                                        /*Aqui instanciamos la clase*/
+                                        $paginacion = new Zebra_Pagination();
+                                        /*Definimos el numero de registros que se quieren mostrar en las tablas*/
+                                        $paginacion->records($totaldatos);
+                                        $paginacion->records_per_page($filas);
+                                        $paginacion->padding(false);
+                                        $sql = "SELECT id_imagen, url, titulo, descripcion FROM imagenes_servicios ORDER BY id_imagen ASC LIMIT " . (($paginacion->get_page() - 1) * $filas) . ', ' . $filas;
                                         $data = "";
                                         foreach($PDO->query($sql) as $row) {
                                             $data .= "<tr>";
                                             $data .= "<td>$row[id_imagen]</td>";
-                                            $data .= "<td>$row[url]</td>";
+                                            $data .= "<td><img src='../$row[url] 'border='0' width='150' height='100'></td>";
                                             $data .= "<td>$row[titulo]</td>";
                                             $data .= "<td>$row[descripcion]</td>";
                                             $data .= "<td>";
@@ -78,6 +96,10 @@ if(!isset($_SESSION['alias']))
                                         ?>
                                         </tbody>
                                     </table>
+                                    <!-- Aqui se imprime la paginacion-->
+                                    <div>
+                                        <?php $paginacion->render();?>
+                                    </div>
                                 </div>
                             </div> <!-- /row -->
 
