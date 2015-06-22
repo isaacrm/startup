17dcc19282ad5bba7fab399fb768e9fd7ec4ac88
isaacrm <mendezisaac.11@gmail.com>
2015-06-22 01:27:14 -0600

    backend updatel

--- backend/controlador.php
+++ backend/controlador.php
@@ -1,16 +1,17 @@
 <?php
 require("bd2.php");
-$sql = "SELECT * FROM empleados";  // sentencia sql
+//Numero de filas de empleados
+$sql = "SELECT * FROM empleados";
 $result = mysql_query($sql);
-$numero = mysql_num_rows($result); // obtenemos el número de filas
-require("bd2.php");
-$sql2 = "SELECT * FROM usuarios";  // sentencia sql
+$numero = mysql_num_rows($result);
+//Numero de filas de usuarios
+$sql2 = "SELECT * FROM usuarios";
 $result2 = mysql_query($sql2);
-$numero2 = mysql_num_rows($result2); // obtenemos el número de filas
-require("bd2.php");
-$sql3 = "SELECT * FROM tipos_usuarios";  // sentencia sql
+$numero2 = mysql_num_rows($result2);
+//Numero de filas de tipos de usuarios
+$sql3 = "SELECT * FROM tipos_usuarios";
 $result3 = mysql_query($sql3);
-$numero3 = mysql_num_rows($result3); // obtenemos el número de filas
+$numero3 = mysql_num_rows($result3);
 
 if ($numero!=0 && $numero2!=0 && $numero3!=0) {
     header("Location: Login.php");
